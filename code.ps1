ef70db71bd47842074bdf30aa622f0372462511a
vovaazure <vovaazure@gmail.com>
2022-11-09 00:38:50 +0400

    ready state

---
+++ code.ps1
@@ -0,0 +1,7 @@
+echo "# azure-devops-aks-kubernetes-terraform-pipeline" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/vovaazure/azure-devops-aks-kubernetes-terraform-pipeline.git
+git push -u origin main
